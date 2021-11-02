e61e4b87616f6a0e96c3cd2ceef74735b2f24f4e
Imaad Uddin <imaaduddin27@gmail.com>
2021-11-02 00:29:44 -0500

    functions

---
+++ Day-8-Functions-With-Inputs/Functions-With-Inputs.py
@@ -0,0 +1,18 @@
+# Functions 
+def my_function():
+    # Do this
+    # Then do this
+    # Finally do this
+
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+
+def greet():
+    print("Hi")
+    print("Hello")
+    print("Hey")
+
+greet()
