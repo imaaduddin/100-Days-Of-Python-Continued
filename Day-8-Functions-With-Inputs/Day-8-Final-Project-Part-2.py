8cdd143b39fa760b0451c169152b25203ce24daa
Imaad Uddin <imaaduddin27@gmail.com>
2021-12-07 00:14:36 -0600

    part 3 of project finished

--- Day-8-Functions-With-Inputs/Day-8-Final-Project-Part-2.py
+++ Day-8-Functions-With-Inputs/Day-8-Final-Project-Part-2.py
@@ -13,7 +13,15 @@ def encrypt(plain_text, shift_amount):
   print(f"The encoded text is {cipher_text}")
 
 #TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
-def decrypt(plain_text, shift_amount):
+def decrypt(cipher_text, shift_amount):
+  blank_text = ""
+  for letter in cipher_text:
+    position = alphabet.index(letter)
+    new_position = position - shift_amount
+    blank_text += alphabet(new_position)
+  print(f"The decoded text is {blank_text}")
+
+
     
     
 
@@ -25,5 +33,8 @@ def decrypt(plain_text, shift_amount):
   #print output: "The decoded text is hello"
 
 
-#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
-encrypt(plain_text=text, shift_amount=shift)
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'direction' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+if direction == "encode":
+  encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+  decrypt(cipher_text=text, shift_amount=shift)
