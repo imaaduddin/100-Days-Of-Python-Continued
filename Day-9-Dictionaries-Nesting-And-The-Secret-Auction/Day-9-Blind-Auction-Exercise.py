ae4a4662ffa0e264862758b4c8e0fd81dea11a82
Imaad Uddin <imaaduddin27@gmail.com>
2021-12-17 00:21:05 -0600

    exercise complete

--- Day-9-Dictionaries-Nesting-And-The-Secret-Auction/Day-9-Blind-Auction-Exercise.py
+++ Day-9-Dictionaries-Nesting-And-The-Secret-Auction/Day-9-Blind-Auction-Exercise.py
@@ -7,12 +7,24 @@ bid_dict = {}
 
 bidding_finished = False
 
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
 while not bidding_finished:
-    name = input("What is your name?")
-    bid = input("What is your bid? $")
+    name = input("What is your name?: ")
+    bid = int(input("What is your bid?: $"))
     bid_dict[name] = bid
-    another_bid = input("Any other bidders? yes or no")
+    another_bid = input("Any other bidders? yes or no \n")
     if another_bid == "no":
         bidding_finished = True
-    else:
+        find_highest_bidder(bid_dict)
+    elif another_bid == "yes":
+        clear()
         
