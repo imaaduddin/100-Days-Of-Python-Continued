dfa96b75efbaa2a36735a31357a4c4c2aa3b2537
Imaad Uddin <imaaduddin27@gmail.com>
2021-12-16 19:11:31 -0600

    my solution so far

---
+++ Day-9-Dictionaries-Nesting-And-The-Secret-Auction/Day-9-Blind-Auction-Exercise.py
@@ -0,0 +1,16 @@
+from Art import logo
+import os
+
+print(logo)
+
+bid_dict = {}
+
+bidding_finished = False
+
+while not bidding_finished:
+    name = input("What is your name?")
+    bid = input("What is your bid? $")
+    bid_dict[name] = bid
+    another_bid = input("Any other bidders? yes or no")
+    if another_bid == "no":
+        bidding_finished = True
