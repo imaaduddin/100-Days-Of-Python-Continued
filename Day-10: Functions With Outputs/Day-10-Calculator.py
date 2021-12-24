568da15ee6615294e431e3d32d8ae91c685912b2
Imaad Uddin <imaaduddin27@gmail.com>
2021-12-23 22:57:44 -0600

    calculator

--- Day-10: Functions With Outputs/Day-10-Calculator.py
+++ Day-10: Functions With Outputs/Day-10-Calculator.py
@@ -1,5 +1,4 @@
 from art import logo
-print(logo)
 
 # Add
 def add(n1, n2):
@@ -24,3 +23,30 @@ operations = {
     "*": multiply,
     "/": divide
 }
+
+def calculator():
+    print(logo)
+    
+    num1 = int(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation from the line above: ")
+        num2 = int(input("What's the second number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation.: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
+
+    
+    
