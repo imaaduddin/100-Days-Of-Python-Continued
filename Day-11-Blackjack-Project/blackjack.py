6a1694a5ec1ce843f43506932811f0c11b3abffd
Imaad Uddin <imaaduddin27@gmail.com>
2021-12-30 19:07:55 -0600

    blackjack game so far

--- Day-11-Blackjack-Project/blackjack.py
+++ Day-11-Blackjack-Project/blackjack.py
@@ -66,10 +66,10 @@ print(logo)
 
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-ace = 11
 
 user_cards = []
 computer_cards = []
+is_game_over = False
 
 def deal_card(cards):
     return random.choice(cards)
@@ -78,10 +78,44 @@ for i in range(2):
     user_cards.append(deal_card(cards))
     computer_cards.append(deal_card(cards))
 
-# print(user_cards)
-# print(computer_cards)
 
 def calculate_score(cards):
     score = sum(cards)
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and score > 21:
+        cards.remove(11)
+        cards.append(1)
     return score
+
+while not is_game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+
+    if user_score or computer_score == 0 or user_score > 21:
+        is_game_over = True
+    else:
+        choice = input("Wanna draw another card? y or n")
+        if choice == "y":
+            user_cards.append(deal_card())
+        else:
+            is_game_over = True
+
+while computer_score != 0 and computer_score < 17:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
+
+def compare(user_score, computer_score):
+    if user_score == computer_score:
+        print("Draw!")
+    elif computer_score == 0 or user_score > 21:
+        print("User loses!")
+    elif user_score == 0 or computer_score > 21:
+        print("Computer loses. You win!")
+    elif:
+        user_score > computer_score:
+            print("You win!")
+    else:
+        print("You lose!")
     
+
