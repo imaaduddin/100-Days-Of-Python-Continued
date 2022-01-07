06c09040ba87eab22f91e62619a243c4ce9f9cd5
Imaad Uddin <imaaduddin27@gmail.com>
2022-01-06 19:06:19 -0600

    number guessing game

--- Day-12-Scope-&-Number-Guessing-Game/Number-Guessing-Game.py
+++ Day-12-Scope-&-Number-Guessing-Game/Number-Guessing-Game.py
@@ -12,3 +12,9 @@
 
 from art import logo
 print(logo)
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100")
+print("Pssst, the correct answer is 17")
+
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
