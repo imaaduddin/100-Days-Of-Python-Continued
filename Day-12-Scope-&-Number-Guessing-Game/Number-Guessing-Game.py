de74acb2567e03f0e2a5483d54b874db18a5cfbe
Imaad Uddin <imaaduddin27@gmail.com>
2022-01-20 18:21:06 -0600

    my solution for the game

--- Day-12-Scope-&-Number-Guessing-Game/Number-Guessing-Game.py
+++ Day-12-Scope-&-Number-Guessing-Game/Number-Guessing-Game.py
@@ -20,6 +20,7 @@ print("Pssst, the correct answer is 17")
 
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
 
+# Easy Choice
 if difficulty == "easy":
     answer = random.randint(1, 100)
     print(answer)
@@ -29,15 +30,44 @@ if difficulty == "easy":
         if guess_one < answer:
             print("Too low.")
             print("Guess again.")
-            print(f"You have {i} attempts remaining to guess the number.")
+            print(f"You have {i - 1} attempts remaining to guess the number.")
             guess_one = int(input("Make a guess: "))
         elif guess_one > answer:
-            print("Too low.")
+            print("Too high.")
             print("Guess again.")
-            print(f"You have {i} attempts remaining to guess the number.")
+            print(f"You have {i - 1} attempts remaining to guess the number.")
             guess_one = int(input("Make a guess: "))
         elif guess_one == answer:
             print(f"You got it! The answer was {answer}")
             break
-    print("You've run out of guesses, you lose.")
+        else:
+            print("You've run out of guesses, you lose.")
+
+# Hard Choice
+if difficulty == "hard":
+    answer = random.randint(1, 100)
+    print(answer)
+    print("You have 5 attempts remaining to guess the number")
+    guess_two = int(input("Make a guess: "))
+    for i in range(5, 1, -1):
+        if guess_two < answer:
+            print("Too low.")
+            print("Guess again.")
+            print(f"You have {i - 1} attempts remaining to guess the number.")
+            guess_two = int(input("Make a guess: "))
+        elif guess_two > answer:
+            print("Too high.")
+            print("Guess again.")
+            print(f"You have {i - 1} attempts remaining to guess the number.")
+            guess_two = int(input("Make a guess: "))
+        elif guess_two == answer:
+            print(f"You got it! The answer was {answer}")
+            break
+        else:
+            print("You've run out of guesses, you lose.")
             
+            
+            
+
+
+          
