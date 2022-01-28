21f236c5c50638cd2694aff8bc26f1aa93779ce0
Imaad Uddin <imaaduddin27@gmail.com>
2022-01-27 18:59:57 -0600

    debugging

---
+++ Day-13: Debugging/Debugging-Odd-Or-Even.py
@@ -0,0 +1,12 @@
+# Instructions
+# Read this the code in main.py
+# Spot the problems 🐞.
+# Modify the code to fix the program.
+
+
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
