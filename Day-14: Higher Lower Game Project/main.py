39cefd8fdef2c0ce47a7f9ba8c15596450187d81
Imaad Uddin <imaaduddin27@gmail.com>
2022-05-23 19:40:06 -0500

    giving user soem feedback

--- Day-14: Higher Lower Game Project/main.py
+++ Day-14: Higher Lower Game Project/main.py
@@ -4,13 +4,23 @@ import random
 
 # Format the account data into printable format
 def format_data(account):
+    # takes the account data and returns the printable format
     account_name = account['name']
     account_description = account['description']
     account_country = account['country']
     return f'{account_name}, a {account_description}, from {account_country}'
 
+# Use if statement to check if user is correct 
+def check_answer(guess, a_followers, b_followers):
+    # Take the user guess and follower counts and returns if they got it right
+    if a_followers > b_followers:
+        return guess == 'a'
+    else:
+        return guess == 'b'
+
 # Display art
 print(logo)
+score = 0
 
 # Generate a random account from the game data
 account_a = random.choice(data)
@@ -29,4 +39,13 @@ guess = input('Who has more followers? Type "A" or "B": ').lower()
 # Get follower count of each account 
 a_follower_count = account_a['follower_count']
 b_follower_count = account_b['follower_count']
-# Use if statement to check if user is correct 
+
+is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+# Giver user some feedback on their guesses + score keeping
+if is_correct:
+    score += 1
+    print(f'You are right! Current score: {score}')
+else:
+    print(f'Sorry that is wrong. Finals score: {score}')
+
