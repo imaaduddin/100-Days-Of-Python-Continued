82e4020b02d601a0e70eb0fca45b494d442fc251
Imaad Uddin <imaaduddin27@gmail.com>
2022-05-23 20:04:42 -0500

    project complete but need to figure out to clearing the game and restarting

--- Day-14: Higher Lower Game Project/main.py
+++ Day-14: Higher Lower Game Project/main.py
@@ -21,31 +21,41 @@ def check_answer(guess, a_followers, b_followers):
 # Display art
 print(logo)
 score = 0
-
-# Generate a random account from the game data
-account_a = random.choice(data)
+game_should_continue = True
 account_b = random.choice(data)
-if account_a == account_b:
+
+# Make the game repeatable 
+while game_should_continue:
+
+    # Generate a random account from the game data
+    account_a = account_b
     account_b = random.choice(data)
+    
+    while account_a == account_b:
+        account_b = random.choice(data)
 
-print(f'Compare A: {format_data(account_a)}.')
-print(vs)
-print(f'Against B: {format_data(account_b)}.')
+    print(f'Compare A: {format_data(account_a)}.')
+    print(vs)
+    print(f'Against B: {format_data(account_b)}.')
 
-# Ask user for a guess 
-guess = input('Who has more followers? Type "A" or "B": ').lower()
+    # Ask user for a guess 
+    guess = input('Who has more followers? Type "A" or "B": ').lower()
 
-# Check if user is correct
-# Get follower count of each account 
-a_follower_count = account_a['follower_count']
-b_follower_count = account_b['follower_count']
+    # Check if user is correct
+    # Get follower count of each account 
+    a_follower_count = account_a['follower_count']
+    b_follower_count = account_b['follower_count']
 
-is_correct = check_answer(guess, a_follower_count, b_follower_count)
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+    
+    # Clear the screen between rounds 
+    clear() # need to fix this
+    print(logo)
 
-# Giver user some feedback on their guesses + score keeping
-if is_correct:
-    score += 1
-    print(f'You are right! Current score: {score}')
-else:
-    print(f'Sorry that is wrong. Finals score: {score}')
-
+    # Giver user some feedback on their guesses + score keeping
+    if is_correct:
+        score += 1
+        print(f'You are right! Current score: {score}')
+    else:
+        game_should_continue = False
+        print(f'Sorry that is wrong. Finals score: {score}')
