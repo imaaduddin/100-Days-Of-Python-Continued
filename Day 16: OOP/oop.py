0b88bdd525227aec8485c1aee5466c605407537a
Imaad Uddin <imaaduddin27@gmail.com>
2022-07-14 15:26:31 -0500

    oop

--- Day 16: OOP/oop.py
+++ Day 16: OOP/oop.py
@@ -1,7 +1,13 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 import another_module
 
 print(another_module.another_variable)
 
 donny = Turtle()
 print(donny)
+donny.shape('turtle')
+donny.color('DarkRed')
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
