2850ee3c82b5b3df99ee31979f3f4d9cd3398c7c
Imaad Uddin <imaaduddin27@gmail.com>
2022-07-14 15:31:18 -0500

    prettytable

--- Day 16: OOP/oop.py
+++ Day 16: OOP/oop.py
@@ -1,13 +1,15 @@
-from turtle import Turtle, Screen
-import another_module
+# from turtle import Turtle, Screen
+# import another_module
 
-print(another_module.another_variable)
+# print(another_module.another_variable)
 
-donny = Turtle()
-print(donny)
-donny.shape('turtle')
-donny.color('DarkRed')
+# donny = Turtle()
+# print(donny)
+# donny.shape('turtle')
+# donny.color('DarkRed')
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
