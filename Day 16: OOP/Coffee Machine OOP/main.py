08f9732ff93185088650ad4c603a08d85c731b28
Imaad Uddin <imaaduddin27@gmail.com>
2022-10-12 17:00:16 -0500

    working on the machine

--- Day 16: OOP/Coffee Machine OOP/main.py
+++ Day 16: OOP/Coffee Machine OOP/main.py
@@ -1,3 +1,10 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+
+ask_user = input('What would you like? (espresso/latte/cappuccino): ')
+
+if ask_user == 'espresso':
+    
